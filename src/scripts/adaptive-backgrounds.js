97280e8b70d1a795859d378c91f42817530d19d6
Amardeep Mann <amar2slick@gmail.com>
2023-02-05 23:03:59 -0800

    BG Color Within Specific Range

--- src/scripts/adaptive-backgrounds.js
+++ src/scripts/adaptive-backgrounds.js
@@ -36,7 +36,47 @@
 
     AdaptiveBackgrounds.prototype.init = function(){
         // Include RGBaster - https://github.com/briangonzalez/rgbaster.js
-        (function(n,t){var a=function(){return document.createElement("canvas").getContext("2d")};var e=function(n,t){var e=new Image;e.crossOrigin="Anonymous";e.src=n.src;e.onload=function(){var r=a();r.drawImage(e,0,0);var o=r.getImageData(0,0,n.width,n.height);t&&t(o.data)}};var r=function(n){return["rgb(",n,")"].join("")};var o=function(n){return n.map(function(n){return r(n.name)})};var i=5;var u=10;var c={};c.colors=function(n,t,a){e(n,function(n){var e=n.length,c={},m="",f=[],l={dominant:{name:"",count:0},palette:Array.apply(null,Array(a||u)).map(Boolean).map(function(n){return{name:"0,0,0",count:0}})};var v=0;while(v<e){f[0]=n[v];f[1]=n[v+1];f[2]=n[v+2];m=f.join(",");if(m in c){c[m]=c[m]+1}else{c[m]=1}if(m!=="0,0,0"&&m!=="255,255,255"){var d=c[m];if(d>l.dominant.count){l.dominant.name=m;l.dominant.count=d}else{l.palette.some(function(n){if(d>n.count){n.name=m;n.count=d;return true}})}}v+=i*4}t&&t({dominant:r(l.dominant.name),palette:o(l.palette)})})};n.RGBaster=n.RGBaster||c})(window);
+        (function(n,t){var a=function(){return document.createElement("canvas").getContext("2d")};var e=function(n,t){var e=new Image;e.crossOrigin="Anonymous";e.src=n.src;e.onload=function(){var r=a();r.drawImage(e,0,0);var o=r.getImageData(0,0,n.width,n.height);t&&t(o.data)}};var r=function(n){return["rgb(",n,")"].join("")};var o=function(n){return n.map(function(n){return r(n.name)})};var i=5;var u=10;
+        // // // The following modification is from ChatGPT
+        var c={};
+        c.colors=function(n,t,a){
+        e(n,function(n){
+            var e=n.length,c={},m="",f=[],l={dominant:{name:"",count:0},palette:Array.apply(null,Array(a||u)).map(Boolean).map(function(n){return{name:"0,0,0",count:0}})};
+            var v=0;
+            while(v<e){
+            f[0]=n[v];
+            f[1]=n[v+1];
+            f[2]=n[v+2];
+            m=f.join(",");
+            if(m in c){
+                c[m]=c[m]+1;
+            }else{
+                c[m]=1;
+            }
+            // Calculate brightness value (based on HSL)
+            var brightness = 0.2126 * f[0] + 0.7152 * f[1] + 0.0722 * f[2];
+            if(m !== "0,0,0" && brightness < 200 && brightness > 50){  // Only count the color if it's not black (0,0,0) and its brightness is below 190
+                var d=c[m];
+                if(d>l.dominant.count){
+                l.dominant.name=m;
+                l.dominant.count=d;
+                }else{
+                l.palette.some(function(n){
+                    if(d>n.count){
+                    n.name=m;
+                    n.count=d;
+                    return true;
+                    }
+                });
+                }
+            }
+            v+=i*4;
+            }
+            t&&t({dominant:r(l.dominant.name),palette:o(l.palette)});
+        });
+        };
+        // // This is where ChatGPT's modifcation ends
+        n.RGBaster=n.RGBaster||c})(window);
     };
 
     AdaptiveBackgrounds.prototype.process = function(){
